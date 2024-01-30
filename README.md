c813b59c9429d50d0d3e9b5083780bf541835341
raja555 <sunilpokhrel797@gmail.com>
2024-01-30 20:05:45 +0545

    folder

---
+++ README.md
@@ -0,0 +1,3 @@
+Project Demo : https://648eaf9db32a7905034665da--teal-muffin-88d1bc.netlify.app/ 
+
+![screencapture-localhost-3000-2023-06-18-13_02_54](https://github.com/sunil9813/Food-Order-Website-/assets/67497228/ad5df655-f61c-4f00-bf64-8e0e90fd7d71)
---
+++ src/components/data/Data.js
@@ -0,0 +1,247 @@
+export const navList = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "pages",
+    path: "/pages",
+  },
+  {
+    text: "shop",
+    path: "/shop",
+  },
+  {
+    text: "blog",
+    path: "/blog",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+];
+export const hero = [
+  {
+    id: 1,
+    title: "GREAT LOCATION",
+    desc: "Rorem ipsum dolor sit amet, etur advoluptatem voluptatem",
+  },
+  {
+    id: 2,
+    title: "NATURE FIRST",
+    desc: "Rorem ipsum dolor sit amet, etur advoluptatem voluptatem",
+  },
+  {
+    id: 3,
+    title: "HEALTHY FOOD",
+    desc: "Rorem ipsum dolor sit amet, etur advoluptatem voluptatem",
+  },
+];
+export const socialIcon = [
+  {
+    icon: <i className="fab fa-facebook-f"></i>,
+    name: "facebook",
+  },
+  {
+    icon: <i className="fab fa-twitter"></i>,
+    name: "twitter",
+  },
+  {
+    icon: <i className="fab fa-youtube"></i>,
+    name: "youtube",
+  },
+  {
+    icon: <i className="fab fa-instagram"></i>,
+    name: "instagram",
+  },
+];
+export const menu = [
+  {
+    id: 1,
+    icon: <i className="fa fa-drumstick-bite"></i>,
+    title: "Chicken",
+    desc: "Rorem ipsum advolu ptateme  volupta tem Rorem ipsuey",
+    cover: "../images/menu/menu1.png",
+  },
+  {
+    id: 2,
+    icon: <i class="fa fa-pizza-slice"></i>,
+    title: "Pizza and drink",
+    desc: "Rorem ipsum advolu ptateme  volupta tem Rorem ipsuey",
+    cover: "../images/menu/menu2.png",
+  },
+  {
+    id: 3,
+    icon: <i class="fa fa-burger"></i>,
+    title: "box meals",
+    desc: "Rorem ipsum advolu ptateme  volupta tem Rorem ipsuey",
+    cover: "../images/menu/menu3.png",
+  },
+  {
+    id: 4,
+    icon: <i class="fa fa-boxes-packing"></i>,
+    title: "combo pack",
+    desc: "Rorem ipsum advolu ptateme  volupta tem Rorem ipsuey",
+    cover: "../images/menu/menu4.png",
+  },
+];
+export const dishes = [
+  {
+    id: 1,
+    cover: "../images/dishes/d1.png",
+    title: "POTATO WEDGES",
+    category: "DRINK",
+    sub: "CHICKEN CHUP",
+    price: 100.0,
+    qty: 1,
+    rate: [<i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>],
+  },
+  {
+    id: 2,
+    cover: "../images/dishes/d2.png",
+    title: "SALMON BURGER",
+    category: "PIZZA",
+    sub: "ICE CREAM",
+    price: 70.0,
+    qty: 1,
+    rate: [<i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>],
+  },
+  {
+    id: 3,
+    cover: "../images/dishes/d3.png",
+    title: "SALMON BURGER",
+    category: "DRINK",
+    sub: "CHICKEN CHUP",
+    price: 20.0,
+    qty: 1,
+    rate: [<i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>],
+  },
+  {
+    id: 4,
+    cover: "../images/dishes/d4.png",
+    title: "SMOKED SALMON",
+    category: "PIZZA",
+    sub: "DRINK",
+    price: 20.0,
+    qty: 1,
+    rate: [<i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>],
+  },
+  {
+    id: 5,
+    cover: "../images/dishes/d5.png",
+    title: "CAULIFLOWER MASH",
+    category: "BURGER",
+    sub: "BLUEBERRY SHAKE",
+    price: 65.0,
+    qty: 1,
+    rate: [<i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>],
+  },
+  {
+    id: 6,
+    cover: "../images/dishes/d6.png",
+    title: "EGG BURGER",
+    category: "DRINK",
+    sub: "CHICKEN CHUP",
+    price: 80.0,
+    qty: 1,
+    rate: [<i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>],
+  },
+];
+export const team = [
+  {
+    id: 1,
+    name: "RASALINA DE",
+    exper: "12 YEAR EXPERIENCE",
+    cover: "../images/team/t1.png",
+  },
+  {
+    id: 2,
+    name: "MARK HENERYTIX",
+    exper: "09 YEAR EXPERIENCE",
+    cover: "../images/team/t2.png",
+  },
+  {
+    id: 3,
+    name: "YLINA PITERSON KIM",
+    exper: "06 YEAR EXPERIENCE",
+    cover: "../images/team/t3.png",
+  },
+];
+export const customer = [
+  {
+    id: 1,
+    cover: "../images/customer/c1.png",
+    icon: <i class="fa fa-quote-right"></i>,
+    desc: "Great food! Fresh, quick, friendly, delicious, affordable! Very flexible with orders. Great service! Great portions! If you want great seafood, this place will not disappoint you. Definitely.",
+    name: "Ronalnd D. Morgan",
+    post: "Founder & co",
+    rate: [<i className="fa fa-star"></i>, <i className="fa fa-star"></i>, <i className="fa fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>],
+    ratenum: "3 Star",
+  },
+  {
+    id: 2,
+    cover: "../images/customer/c2.png",
+    icon: <i class="fa fa-quote-right"></i>,
+    desc: "Great food! Fresh, quick, friendly, delicious, affordable! Very flexible with orders. Great service! Great portions! If you want great seafood, this place will not disappoint you. Definitely.",
+    name: "Rasalina De",
+    post: "Developer",
+    rate: [<i className="fa fa-star"></i>, <i className="fa fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>, <i className="far fa-star"></i>],
+    ratenum: "2 Star",
+  },
+];
+export const cdata = [
+  {
+    num: "14k+",
+    name: "HAPPY CUSTOMER",
+  },
+  {
+    num: "16k+",
+    name: "AWARD WINING",
+  },
+  {
+    num: "68k+",
+    name: "FOOD MENU",
+  },
+];
+export const blog = [
+  {
+    id: 1,
+    date: "JAN 15, 2022",
+    name: "BEST BURGER IN YOUR SOCI...",
+    cover: "../images/blog/b1.jpg",
+  },
+  {
+    id: 2,
+    date: "JAN 15, 2022",
+    name: "12 SPEEDY SEAFOOD RECIPES..",
+    cover: "../images/blog/b2.jpg",
+  },
+  {
+    id: 3,
+    date: "JAN 15, 2022",
+    name: "WHO’S WINNING THE FAST...",
+    cover: "../images/blog/b3.jpg",
+  },
+  {
+    id: 1,
+    date: "JAN 15, 2022",
+    name: "BEST BURGER IN YOUR SOCI...",
+    cover: "../images/blog/b1.jpg",
+  },
+  {
+    id: 2,
+    date: "JAN 15, 2022",
+    name: "12 SPEEDY SEAFOOD RECIPES..",
+    cover: "../images/blog/b2.jpg",
+  },
+  {
+    id: 3,
+    date: "JAN 15, 2022",
+    name: "WHO’S WINNING THE FAST...",
+    cover: "../images/blog/b3.jpg",
+  },
+];
